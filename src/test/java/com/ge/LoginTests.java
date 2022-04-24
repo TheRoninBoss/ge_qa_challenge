ee5d7ded9892d9a89da105f4a55b5031a048e964
Eduardo Enciso <edward.c.trb@gmail.com>
2022-04-24 16:35:36 -0500

    Add unathorizedUserTest

--- src/test/java/com/ge/LoginTests.java
+++ src/test/java/com/ge/LoginTests.java
@@ -6,12 +6,25 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import static com.codeborne.selenide.Selenide.page;
+import static org.testng.Assert.assertEquals;
 
 public class LoginTests extends BaseTest{
+
+    //TODO: use data provider.
+    private static final String BAD_CREDS_ERROR_MSG = "Epic sadface: Username and password do not match any user in this service";
+
 
     @Test
     public void loginTest() {
         ProductsPage productsPage = page(LoginPage.class).login("standard_user","secret_sauce");
-        Assert.assertEquals(productsPage.getTitle(), "PRODUCTS");
+        assertEquals(productsPage.getTitle(), "PRODUCTS");
+    }
+
+    @Test
+    public void unauthorizedUserTest() throws InterruptedException {
+        LoginPage loginPage = page(LoginPage.class);
+        loginPage.login("not_an_user", "secret_sauce");
+        String errorMsg = loginPage.getBadCredsMsg();
+        assertEquals(errorMsg,BAD_CREDS_ERROR_MSG);
     }
 }
