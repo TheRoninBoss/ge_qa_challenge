185bbe957c9f3fefbd927cf340f02c65f8cf57b8
Eduardo Enciso <edward.c.trb@gmail.com>
2022-04-25 12:39:14 -0500

    Add logoutTest to LoginTests

--- src/test/java/com/ge/LoginTests.java
+++ src/test/java/com/ge/LoginTests.java
@@ -2,36 +2,40 @@ package com.ge;
 
 import com.ge.pageobject.LoginPage;
 import com.ge.pageobject.ProductsPage;
-import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import static com.codeborne.selenide.Selenide.page;
 import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertNotNull;
 
-public class LoginTests extends BaseTest{
+public class LoginTests extends BaseTest {
 
     //TODO: use data provider.
-    private static final String BAD_CREDS_ERROR_MSG = "Epic sadface: Username and password do not match any user in this service";
+    private static final String BAD_CREDS_ERROR_MSG =
+            "Epic sadface: Username and password do not match any user in this service";
 
 
-//    @Test
+    @Test
     public void loginTest() {
-        ProductsPage productsPage = page(LoginPage.class).login("standard_user","secret_sauce");
+        ProductsPage productsPage =
+                page(LoginPage.class).login("standard_user", "secret_sauce");
         assertEquals(productsPage.getTitle(), "PRODUCTS");
     }
 
-//    @Test
+    @Test
     public void unauthorizedUserTest() {
         LoginPage loginPage = page(LoginPage.class);
         loginPage.login("not_an_user", "secret_sauce");
         String errorMsg = loginPage.getBadCredsMsg();
-        assertEquals(errorMsg,BAD_CREDS_ERROR_MSG);
+        assertEquals(errorMsg, BAD_CREDS_ERROR_MSG);
     }
 
     @Test
     public void logoutTest() {
-        ProductsPage productsPage = page(LoginPage.class).login("standard_user","secret_sauce");
-        productsPage.printInventory();
-        assertEquals(productsPage.getInventoryItemQty(), 6);
+        LoginPage loginPage =
+                page(LoginPage.class)
+                        .login("standard_user", "secret_sauce")
+                        .logout();
+        assertNotNull(loginPage);
     }
 }
