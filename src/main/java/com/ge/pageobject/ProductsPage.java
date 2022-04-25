e8b3f7804dedecd02f2b7de32ff1e8edb5bfca75
Eduardo Enciso <edward.c.trb@gmail.com>
2022-04-25 11:38:10 -0500

    Refactor ProductsPage. InventoryItem page fragment is working now

--- src/main/java/com/ge/pageobject/ProductsPage.java
+++ src/main/java/com/ge/pageobject/ProductsPage.java
@@ -1,29 +1,17 @@
 package com.ge.pageobject;
 
-import com.codeborne.selenide.ElementsCollection;
-import com.codeborne.selenide.ElementsContainer;
 import com.codeborne.selenide.SelenideElement;
-import com.codeborne.selenide.impl.ElementsContainerCollection;
 import org.openqa.selenium.support.FindBy;
 
-public class ProductsPage extends BasePage{
+import java.util.List;
 
-    @FindBy(className = "inventory_list")
-    private ElementsCollection inventoryItems;
+public class ProductsPage extends BasePage {
+
+    @FindBy(className = "inventory_item")
+    private List<InventoryItem> inventoryItems;
 
     @FindBy(className = "select_container")
     private SelenideElement filterDropdown;
-
-    public enum SortOrder {
-        NAME_A_TO_Z("Name (A to Z)"),
-        NAME_Z_TO_A("Name (Z to A)"),
-        PRICE_LOW_TO_HIGH("Price (low to high)"),
-        PRICE_HIGH_TO_LOW("Price (high to low)");
-        String textValue;
-        SortOrder(String textValue){
-            this.textValue = textValue;
-        }
-    }
 
     public ProductsPage sortInventory(SortOrder desiredOrder) {
         //TODO: First check what's the current sort order.
@@ -31,7 +19,19 @@ public class ProductsPage extends BasePage{
         return this;
     }
 
-    class InventoryItem extends ElementsContainer{
+    public enum SortOrder {
+        NAME_A_TO_Z("Name (A to Z)"),
+        NAME_Z_TO_A("Name (Z to A)"),
+        PRICE_LOW_TO_HIGH("Price (low to high)"),
+        PRICE_HIGH_TO_LOW("Price (high to low)");
+        String textValue;
+
+        SortOrder(String textValue) {
+            this.textValue = textValue;
+        }
+    }
+
+    static class InventoryItem extends AbstractPageFragment {
 
         @FindBy(className = "inventory_item_name")
         private SelenideElement inventoryItemNameLabel;
