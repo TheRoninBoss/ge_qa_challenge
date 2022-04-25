67f9aa9f797cbec3a27a409206c03ca8c03f1ed6
Eduardo Enciso <edward.c.trb@gmail.com>
2022-04-25 08:39:51 -0500

    Change BeforeTest and AfterTest annotations for BeforeMethod and AfterMethod to run more than 1 test per session

--- src/test/java/com/ge/BaseTest.java
+++ src/test/java/com/ge/BaseTest.java
@@ -1,20 +1,20 @@
 package com.ge;
 
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 
 import static com.codeborne.selenide.Selenide.closeWebDriver;
 import static com.codeborne.selenide.Selenide.open;
 
 public abstract class BaseTest {
 
-    @BeforeTest
+    @BeforeMethod
     public void setUp(){
         open("https://www.saucedemo.com/");
     }
 
-    @AfterTest
-    public void tearDown(){
+    @AfterMethod
+    public void tearDown() {
         closeWebDriver();
     }
 }
