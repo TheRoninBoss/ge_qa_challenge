e093764c87e8d3e56b7aca4d6f584313e8fc2026
Eduardo Enciso <edward.c.trb@gmail.com>
2022-04-25 12:37:17 -0500

    Implement burger menu logout in AbstractBasePage

--- src/main/java/com/ge/pageobject/AbstractBasePage.java
+++ src/main/java/com/ge/pageobject/AbstractBasePage.java
@@ -1,7 +1,12 @@
 package com.ge.pageobject;
 
+import com.codeborne.selenide.ElementsCollection;
 import com.codeborne.selenide.SelenideElement;
 import org.openqa.selenium.support.FindBy;
+
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Selenide.Wait;
+import static com.codeborne.selenide.Selenide.page;
 
 public abstract class AbstractBasePage {
 
@@ -11,10 +16,51 @@ public abstract class AbstractBasePage {
     @FindBy(id = "shopping_cart_container")
     private SelenideElement shoppingCartButton;
 
+    @FindBy(className = "bm-menu")
+    private BurgerMenu burgerMenu;
+
     @FindBy(className = "title")
     private SelenideElement title;
 
     public String getTitle() {
         return title.text();
+    }
+
+    public LoginPage logout() {
+        burgerMenuButton.click();
+        burgerMenu.logout();
+        return page(LoginPage.class);
+    }
+
+    public enum BurgerMenuOptions {
+        ALL_ITEMS("ALL ITEMS"),
+        ABOUT("ABOUT"),
+        LOGOUT("LOGOUT"),
+        RESET_APP_STATE("RESET APP STATE");
+
+        private String text;
+
+        BurgerMenuOptions(String text) {
+            this.text = text;
+        }
+
+        public String getText() {
+            return this.text;
+        }
+    }
+
+    static class BurgerMenu extends AbstractPageFragment {
+
+        @FindBy(className = "bm-item")
+        private ElementsCollection burgerMenuItems;
+
+        private void logout() {
+            selectOption(BurgerMenuOptions.LOGOUT);
+        }
+
+        private void selectOption(BurgerMenuOptions option) {
+            Wait().until(webDriver -> this.getSelf().isDisplayed());
+            burgerMenuItems.filter(exactText(option.getText())).first().click();
+        }
     }
 }
--- src/test/java/com/ge/LoginTests.java
+++ src/test/java/com/ge/LoginTests.java
@@ -14,17 +14,24 @@ public class LoginTests extends BaseTest{
     private static final String BAD_CREDS_ERROR_MSG = "Epic sadface: Username and password do not match any user in this service";
 
 
-    @Test
+//    @Test
     public void loginTest() {
         ProductsPage productsPage = page(LoginPage.class).login("standard_user","secret_sauce");
         assertEquals(productsPage.getTitle(), "PRODUCTS");
     }
 
-    @Test
+//    @Test
     public void unauthorizedUserTest() {
         LoginPage loginPage = page(LoginPage.class);
         loginPage.login("not_an_user", "secret_sauce");
         String errorMsg = loginPage.getBadCredsMsg();
         assertEquals(errorMsg,BAD_CREDS_ERROR_MSG);
+    }
+
+    @Test
+    public void logoutTest() {
+        ProductsPage productsPage = page(LoginPage.class).login("standard_user","secret_sauce");
+        productsPage.printInventory();
+        assertEquals(productsPage.getInventoryItemQty(), 6);
     }
 }
