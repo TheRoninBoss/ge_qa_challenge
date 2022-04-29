43ffbd3e7e85763032e7f1d9ce251393cbe413b5
Eduardo Enciso <edward.c.trb@gmail.com>
2022-04-28 22:41:53 -0500

    Implement Selenide reports

--- src/test/java/com/ge/BaseTest.java
+++ src/test/java/com/ge/BaseTest.java
@@ -1,14 +1,17 @@
 package com.ge;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.testng.GlobalTextReport;
 import com.ge.core.TestData;
 import com.ge.pageobject.LoginPage;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Listeners;
 import org.testng.annotations.Parameters;
 
 import static com.codeborne.selenide.Selenide.*;
 
+@Listeners({GlobalTextReport.class})
 public abstract class BaseTest {
 
     protected static final TestData TEST_DATA = TestData.getInstance();
